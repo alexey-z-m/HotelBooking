1c69e75c357e27be98378968baeeeb1af5ba104f
alexey-z-m <alexey.panda@gmail.com>
2023-09-01 10:35:03 +0300

    init commit

---
+++ HotelBooking/HotelBooking/HotelBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingApp.swift
+//  HotelBooking
+//
+//  Created by Alexey Zablotskiy on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
